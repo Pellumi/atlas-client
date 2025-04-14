d8b41697427c9ed9b044142869be7f0f7f5ff31c
Pellumi <pellumii67@gmail.com>
2025-04-14 08:59:32 +0100

    feat: Add utility function to convert ISO date strings to readable date format

---
+++ src/utiils/minorUtils.js
@@ -0,0 +1,11 @@
+export function convertToReadableDateTime(isoString) {
+  const date = new Date(isoString);
+  return date.toLocaleString(undefined, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
